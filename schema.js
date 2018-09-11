47349672bf2f7d2da08a73c416ebf33cd1de42b7
JadTermsani <jadtermsani@gmail.com>
2018-09-11 19:10:09 +0300

    fix schema

--- schema.js
+++ schema.js
@@ -46,7 +46,7 @@ const typeDefs = gql`
     top10s: Int
     suicides: Int
     teamKills: Int
-    kdRatio: Int
+    kdRatio: Float
     runningDistance: Int
     drivingDistance: Int
     vehiclesDestroyed: Int
@@ -54,7 +54,7 @@ const typeDefs = gql`
     revives: Int
     damage: Int
     mostKills: Int
-    longestKill: Int
+    longestKill: Float
     timePlayed: Int
     longestGame: Int
   }
@@ -259,48 +259,66 @@ const resolvers = {
           },
           idx,
           arr
-        ) => ({
-          kills: accum.kills ? accum.kills + kills : kills,
-          assists: accum.assists ? accum.assists + assists : assists,
-          losses: accum.losses ? accum.losses + losses : losses,
-          roundsPlayed: accum.roundsPlayed
-            ? accum.roundsPlayed + roundsPlayed
-            : roundsPlayed,
-          wins: accum.wins ? accum.wins + wins : wins,
-          top10s: accum.top10s ? accum.top10s + top10s : top10s,
-          suicides: accum.suicides ? accum.suicides + suicides : suicides,
-          teamKills: accum.teamKills ? accum.teamKills + teamKills : teamKills,
-          kdRatio:
-            idx === arr.length - 1
-              ? (accum.kills + kills) / (accum.losses + losses)
-              : 0,
-          runningDistance: accum.walkDistance
-            ? accum.walkDistance + walkDistance
-            : walkDistance,
-          drivingDistance: accum.rideDistance
-            ? accum.rideDistance + rideDistance
-            : rideDistance,
-          vehiclesDestroyed: accum.vehicleDestroys
-            ? accum.vehicleDestroys + vehicleDestroys
-            : vehicleDestroys,
-          heals: accum.heals ? accum.heals + heals + boosts : heals + boosts,
-          revives: accum.revives ? accum.revives + revives : revives,
-          damage: accum.damageDealt
-            ? accum.damageDealt + damageDealt
-            : damageDealt,
-          mostKills: accum.roundMostKills
-            ? Math.max(accum.roundMostKills, roundMostKills)
-            : roundMostKills,
-          longestKill: accum.longestKill
-            ? Math.max(accum.longestKill, longestKill)
-            : longestKill,
-          timePlayed: accum.timeSurvived
-            ? accum.timeSurvived + timeSurvived
-            : timeSurvived,
-          longestGame: accum.longestTimeSurvived
-            ? Math.max(accum.longestTimeSurvived, longestTimeSurvived)
-            : longestTimeSurvived
-        }),
+        ) => {
+          return {
+            kills: accum.kills ? accum.kills + kills : kills,
+            assists: accum.assists ? accum.assists + assists : assists,
+            deaths: accum.deaths ? accum.deaths + losses : losses,
+            rounds: accum.rounds ? accum.rounds + roundsPlayed : roundsPlayed,
+            wins: accum.wins ? accum.wins + wins : wins,
+            top10s: accum.top10s ? accum.top10s + top10s : top10s,
+            suicides: accum.suicides ? accum.suicides + suicides : suicides,
+            teamKills: accum.teamKills
+              ? accum.teamKills + teamKills
+              : teamKills,
+            kdRatio: parseFloat(
+              idx === arr.length - 1
+                ? (accum.kills + kills) / (accum.deaths + losses)
+                : 0
+            ).toFixed(2),
+            runningDistance: parseInt(
+              accum.runningDistance
+                ? accum.runningDistance + walkDistance
+                : walkDistance,
+              10
+            ),
+            drivingDistance: parseInt(
+              accum.drivingDistance
+                ? accum.drivingDistance + rideDistance
+                : rideDistance,
+              10
+            ),
+            vehiclesDestroyed: accum.vehiclesDestroyed
+              ? accum.vehiclesDestroyed + vehicleDestroys
+              : vehicleDestroys,
+            heals: accum.heals ? accum.heals + heals + boosts : heals + boosts,
+            revives: accum.revives ? accum.revives + revives : revives,
+            damage: parseInt(
+              accum.damage ? accum.damage + damageDealt : damageDealt,
+              10
+            ),
+            mostKills: accum.mostKills
+              ? Math.max(accum.mostKills, roundMostKills)
+              : roundMostKills,
+            longestKill: parseFloat(
+              accum.longestKill
+                ? Math.max(accum.longestKill, longestKill)
+                : longestKill
+            ).toFixed(2),
+            timePlayed: parseInt(
+              accum.timePlayed
+                ? accum.timePlayed + timeSurvived / 60
+                : timeSurvived / 60,
+              10
+            ),
+            longestGame: parseInt(
+              accum.longestGame
+                ? Math.max(accum.longestGame, longestTimeSurvived)
+                : longestTimeSurvived,
+              10
+            )
+          };
+        },
         {}
       );
 
